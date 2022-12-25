f46d0b21ede9b4f539004fcb2b2c34fbedba289c
Mohanraj K <mohanraj.kandasamy@kohler.com>
2022-12-25 19:53:39 +0530

    core components

--- dist/cjs/atoms/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react';
-export interface IButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    backgroundColor?: string;
-    color?: string;
-    label: string;
-}
-export declare const Button: FC<IButtonProps>;
